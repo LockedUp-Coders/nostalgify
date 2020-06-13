bd83bb3a678fc63e439ce22f46715fc7d6dd4232
Vivek Shah <vivekshah9969@gmail.com>
2020-06-13 23:56:35 +0530

    changed DB drive to mongoose

--- src/modules/database/mongo.ts
+++ src/modules/database/mongo.ts
@@ -1,26 +1,22 @@
-import { MongoClient } from 'mongodb'
+import mongoose from 'mongoose'
 import logger from '../logger/winston'
 
+// port number is compulsary in mongoose connectionString
 const connectionString: string =
   process.env.connection_string || 'mongodb://127.0.0.1:27017/'
 
 // eslint-disable-next-line
 let mongodb: any
+let dbName:string = "nostalgify"
 
 function connect(callback: any) {
-  MongoClient.connect(
-    connectionString,
-    { useUnifiedTopology: true },
-    (err, client) => {
-      if (err) {
-        logger.error('Error Connecting to database')
-      }
-
-      const db = client.db('attendance')
-      mongodb = db
-      callback()
-    },
-  )
+  mongodb = mongoose.connect(connectionString + dbName,
+              { useNewUrlParser: true, useUnifiedTopology: true}, 
+              function(error) {
+                logger.error('Error Connecting to database');
+                return null;
+              });
+  callback()
 }
 
 function close() {
